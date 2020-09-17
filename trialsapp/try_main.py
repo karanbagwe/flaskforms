3cd0026a7141c930f0e07388aeb53f41ecc5cf2f
karanbagwe <71319362+karanbagwe@users.noreply.github.com>
2020-09-17 21:21:25 +0530

    removed useless files

---
+++ trialsapp/try_main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, RadioField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+    function = RadioField('function', choices=[('ADD','insert'),('UPDATE','change'),('DELETE','remove')])
+
+
+@app.route("/", methods=['GET','POST'])
+def form():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return render_template('try_submit.html', \
+                               username=form.username.data, password=form.password.data, \
+                               addfunc=form.addfunc.data, \
+                               changefunc=form.changefunc.data,\
+                               removefunc=form.removefunc.data)
+#       return '<h1>The username is {}.<br><h1>The password is {}.<br><h5> SUBMITTED !!'.format(form.username.data, form.password.data)
+    return render_template('try_form.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
