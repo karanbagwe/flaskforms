5c89c5a20fdeb60e22307418c2fa4a58dbd4b4b1
karanbagwe <71319362+karanbagwe@users.noreply.github.com>
2020-09-17 20:38:44 +0530

    removed useless files

--- formapp/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class UserForm(FlaskForm):
-    fname = StringField("First Name")
-    lname = StringField("Last Name")
-    email = StringField("Email")
-    submit = SubmitField("Submit")
