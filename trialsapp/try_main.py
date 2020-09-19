05264f60ce7725166ffa75fc4d5f4d1720abbbbf
karanbagwe <71319362+karanbagwe@users.noreply.github.com>
2020-09-20 01:21:06 +0530

    Name,Place,Animal,Movie

--- trialsapp/try_main.py
+++ trialsapp/try_main.py
@@ -6,35 +6,40 @@ import pandas as pd
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
-df=pd.DataFrame()
+
+
 
 class LoginForm(FlaskForm):
 #    username = StringField('username')
 #    password = PasswordField('password')
 #    function = RadioField('function', choices=[('insert','ADD'),('update','CHANGE'),('delete','REMOVE')])
-    fname = StringField('fname')
-    place = PasswordField('place')
-    animal = RadioField('animal', choices=[('Lion','Lion'),('Eagle','Eagle'),('Shark','Shark')])
-    movie = StringField('movie')
+    fname = StringField('Enter Name: ')
+    place = StringField('Enter Place: ')
+    animal = RadioField('Enter Animal: ', choices=[('Lion','Lion'),('Eagle','Eagle'),('Shark','Shark')])
+    movie = StringField('Enter Movie: ')
 
 @app.route("/", methods=['GET','POST'])
 def form():
     form = LoginForm()
     if form.validate_on_submit():
-        dfadd = pd.DataFrame({"Name":[form.fname.data],
-                              "Place":[form.place.data],
-                              "Animal":[form.animal.data],
-                              "Movie":[form.movie.data]})
-        df.append(dfadd)
+        if 'df' not in globals():
+            df = pd.DataFrame(columns=['Name', 'Place', 'Animal', 'Movie'])
+        df = df.append({"Name":format(form.fname.data),
+                        "Place":format(form.place.data),
+                        "Animal":format(form.animal.data),
+                        "Movie":format(form.movie.data)},
+                       ignore_index=True)
         print(df)
+        df.to_csv(r"C:\Users\USER\PycharmProjects\Web_Forms_VBB\trialsapp\data_files\data.csv",mode='a',header=False)
 #        datafile=open(r"C:\Users\USER\PycharmProjects\Web_Forms_VBB\trialsapp\data_files\data.xls","w+")
 #        datafile.write(form.username.data)
 #        datafile.write(form.password.data)
 #        datafile.write(form.function.data)
 #        datafile.close()
         return render_template('try_submit.html', \
-                               fname=form.fname.data, place=form.place.data, \
-                               animal=form.animal.data,movie=form.movie.data)
+                               df=df)
+#                               fname=form.fname.data, place=form.place.data, \
+#                               animal=form.animal.data,movie=form.movie.data)
 #       return '<h1>The username is {}.<br><h1>The password is {}.<br><h5> SUBMITTED !!'.format(form.username.data, form.password.data)
     return render_template('try_form.html', form=form)
 
